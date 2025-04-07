6355b2b6ace2dabc678e34355b4d43d099995b19
Joshua Nwajei <nwajeijoshua55@gmail.com>
2025-04-07 21:46:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Weather from "./Component/Weather";
+import './index.css'
+function App() {
+  return (
+    <div className="app">
+    
+    <Weather/>
+    </div>
+  );
+};
+
+export default App
