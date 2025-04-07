197877514f8d76e2cb65cb7836122493b06f46ce
Joshua Nwajei <nwajeijoshua55@gmail.com>
2025-04-07 21:58:42 +0100

    second commit

--- src/Component/Weather.jsx
+++ src/Component/Weather.jsx
@@ -34,7 +34,7 @@ const allIcons ={
 }
   const search = async(city)=>{
     try {
-      const url =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&unit=metric&appid=${import.meta.env.VITE_APP_ID} `;
+      const url =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID} `;
       const response = await fetch(url);
       const data = await response.json();
       console.log(data);
@@ -61,7 +61,7 @@ useEffect(()=>{
         <img src={search_icon} alt="" onClick={()=>search(inputRef.current.value)} />
        </div>
        <img src={weatherData.icon} alt="" className='weather-icon' />
-       <p className='temperature'> {weatherData.temperature}</p>
+       <p className='temperature'> {weatherData.temperature}°c</p>
        <p  className='location'>{weatherData.location}</p>
        <div className="weather-data">
         <div className="col">
